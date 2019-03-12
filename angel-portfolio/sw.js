1e252d38b979836e17ced9b7d03b5b1ad06b7440
amurillojr92 <amurillojr92@gmail.com>
2019-03-12 03:55:36 -0400

    added portfolio

---
+++ angel-portfolio/sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('vanjstodo-store').then(function(cache) {
+     return cache.addAll([
+       '/angel-portfolio/',
+       '/angel-portfolio/index.html',
+       '/angel-portfolio/index.js',
+       '/angel-portfolio/images/pirate_PNG90.png'
+       
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
